376f8dabaaec77697b261129fc2855be85b35a39
kimhyesuu <you@example.com>
2020-09-30 23:11:01 +0900

    NEW

---
+++ HSMes/HSUiModel/Enum/PageControl/ChangePage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HSUiModel.Enum.PageControl
+{
+   public enum IndexPages
+   {
+      ProcessManagement,
+      ProductionManagement,      
+   }
+
+   public class ChangePage
+   {
+      public IndexPages CurrentView { get; set; }
+   }
+}
---
+++ HSMes/HSViewModel/MainViewModel.cs
@@ -0,0 +1,37 @@
+﻿using HSFramework.Base;
+using HSUiModel.Enum.PageControl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace HSViewModel
+{
+   public class MainViewModel : BaseViewModel
+   {
+
+      private readonly ChangePage _indexPages;
+
+      public ICommand PageControlCommand { set; private get; }
+
+      public IndexPages CurrentView
+      {
+         get => _indexPages.CurrentView;
+         set
+         {
+            _indexPages.CurrentView = value;
+            OnPropertyChanged(nameof(CurrentView));
+         }
+      }
+
+      public MainViewModel()
+      {
+         PageControlCommand = new RelayCommand(o => OnSwithPage(o));
+      }
+
+      public void OnSwithPage(object indexPages) => CurrentView = (IndexPages)Enum.Parse(typeof(IndexPages), indexPages.ToString());
+
+   }
+}
