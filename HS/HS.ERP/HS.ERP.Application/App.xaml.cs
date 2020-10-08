16bfe3184ca2e473ffe192b3d46f0819b31b2e55
kimhyesu <juid00@naver.com>
2020-10-08 23:10:09 +0900

    NEW

---
+++ HS/HS.ERP/HS.ERP.Application/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using HS.ERP.ViewModel.ViewModel;
+using System.Windows;
+
+namespace HS.ERP.Application
+{
+   /// <summary>
+   /// App.xaml에 대한 상호 작용 논리
+   /// </summary>
+   public partial class App 
+   {
+      protected override void OnStartup(StartupEventArgs e)
+      {
+         base.OnStartup(e);
+         View.MainWindowView view = new View.MainWindowView();
+         view.DataContext = new MainViewModel();
+         view.Show();
+      }
+   }
+}
---
+++ HS/HS.ERP/HS.ERP.ViewModel/ViewModel/MainViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HS.ERP.ViewModel.ViewModel
+{
+   public class MainViewModel
+   {
+   }
+}
